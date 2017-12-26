7d2eb7ee31d570854cc395915750d3a247af402b
Petri Autio <petri.mikael.autio@gmail.com>
2017-12-26 22:53:20 +0000

    initial commit

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour {
+    public Transform targetObject;
+    public bool lockToPlayer = true;
+    private Transform playerObject;
+    // delayed? 
+    // for performance could update this only sometimes?
+    // Use this for initialization
+    void Start () {
+        playerObject = GameObject.Find("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(targetObject);
+        if(lockToPlayer)
+        {
+            transform.LookAt(playerObject);
+        }
+	}
+}
---
+++ Assets/Scripts/ThrowFeed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowFeed : MonoBehaviour {
+    public int seeds = 20;
+    public float force = 1.0f;
+    public Transform seedObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.F))
+        {
+            for (int i = 0; i < seeds; i++)
+            {
+                Vector3 offset = new Vector3(Random.Range(-0.2f, 0.2f), Random.Range(-0.2f, 0.2f), Random.Range(-0.2f, 0.2f));
+                Transform newSeed = Instantiate(seedObject, transform.position + offset, Quaternion.identity);
+                newSeed.parent = this.transform;
+                newSeed.GetComponent<Rigidbody>().AddExplosionForce(force, transform.position, 5.0f); 
+            }
+
+        }
+	}
+}
