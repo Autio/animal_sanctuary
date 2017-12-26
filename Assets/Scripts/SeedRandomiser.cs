8c1961224133369a9a1e74ec7f5eebc4b0180a75
Petri Autio <petri.mikael.autio@gmail.com>
2017-12-26 23:30:38 +0000

    Seed throwing

---
+++ Assets/Scripts/SeedRandomiser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedRandomiser : MonoBehaviour {
+    public Sprite[] Sprites;
+	// Use this for initialization
+	void Start () {
+        int r = Random.Range(0, Sprites.Length);
+        
+            this.GetComponent<SpriteRenderer>().sprite = Sprites[r];
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/ThrowFeed.cs
+++ Assets/Scripts/ThrowFeed.cs
@@ -6,9 +6,10 @@ public class ThrowFeed : MonoBehaviour {
     public int seeds = 20;
     public float force = 1.0f;
     public Transform seedObject;
+    private Transform seedParent;
 	// Use this for initialization
 	void Start () {
-		
+        seedParent = GameObject.Find("Seeds").transform;
 	}
 	
 	// Update is called once per frame
@@ -17,10 +18,11 @@ public class ThrowFeed : MonoBehaviour {
         {
             for (int i = 0; i < seeds; i++)
             {
-                Vector3 offset = new Vector3(Random.Range(-0.2f, 0.2f), Random.Range(-0.2f, 0.2f), Random.Range(-0.2f, 0.2f));
+                Vector3 offset = new Vector3(Random.Range(0.2f, 0.6f), Random.Range(0.1f, 0.4f), Random.Range(0.5f, 1.5f));
                 Transform newSeed = Instantiate(seedObject, transform.position + offset, Quaternion.identity);
-                newSeed.parent = this.transform;
-                newSeed.GetComponent<Rigidbody>().AddExplosionForce(force, transform.position, 5.0f); 
+                newSeed.parent = seedParent;
+                newSeed.GetComponent<Rigidbody>().AddExplosionForce(force, transform.position, 5.0f);
+                newSeed.GetComponent<Rigidbody>().AddForce(Vector3.forward * force);
             }
 
         }
