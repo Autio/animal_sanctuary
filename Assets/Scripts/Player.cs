92fad55ca64c915eb33d4e8a8c0a0080e0eb487e
Petri Autio <petri.mikael.autio@gmail.com>
2017-12-28 22:40:34 +0000

    Radio playing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,20 +4,54 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    class heldObjectData : MonoBehaviour
+    {
+        public bool lookAt;
+        public bool lookAtPlayer;
+        
+    }
 
     GameObject heldObject;
+    GameObject heldObjectPlace;
+    heldObjectData hod = new heldObjectData();
+    float heldObjectSize;
 	// Use this for initialization
 	void Start () {
-		
+        heldObjectPlace = GameObject.Find("Hold");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (heldObject == null)
+            {
+
+                TryGrabObject(GetMouseHoverObject(5));
+            }
+            else
+            {
+                TryDropObject();
+            }
+        }
+
+        if(heldObject != null)
+        {
+            Vector3 newPosition = heldObjectPlace.transform.position;
+            heldObject.transform.position = newPosition;
+        }
+
+
+
+
 		if(Input.GetKeyDown(KeyCode.T))
         {
             heldObject = GameObject.Find("Hold");
             Throw(700.0f);
         }
+
+       // Debug.Log(GetMouseHoverObject(5.0f));
 	}
 
     public void SetHeldObject(GameObject g)
@@ -29,9 +63,44 @@ public class Player : MonoBehaviour {
         return heldObject;
     }
 
-    public void Pickup()
+    GameObject GetMouseHoverObject(float range)
     {
+        int layerMask = 1 << 2;
+        layerMask = ~layerMask;
+        Vector3 position = GameObject.Find("FirstPersonCharacter").transform.position;
+        RaycastHit raycastHit;
+        Vector3 target = position + Camera.main.transform.forward * range;
+        if (Physics.Linecast(position, target, out raycastHit, layerMask))
+            Debug.Log("Picking up object " + raycastHit.collider.gameObject.name);
+            return raycastHit.collider.gameObject;
+        return null;
         // try and pick up the object in front of you
+    }
+
+    void TryGrabObject(GameObject grabObject)
+    {
+        if (grabObject == null)
+            return;
+
+        heldObject = grabObject;
+
+        try
+        {
+            heldObject.transform.GetComponent<LookAt>().enabled = true;
+            heldObject.transform.GetComponent<LookAt>().lockToPlayer = true;
+        }
+        catch
+        {
+            Debug.Log("Grabbed object did not have a lookat script");
+        }
+    }
+
+    void TryDropObject()
+    {
+        if (heldObject == null)
+            return;
+        ObjectRevert();
+        heldObject = null;
 
     }
     public void Throw(float force)
@@ -45,9 +114,71 @@ public class Player : MonoBehaviour {
         heldObject.transform.parent = null;
         heldObject.GetComponent<Rigidbody>().AddForce((velocity + GameObject.Find("FirstPersonCharacter").transform.forward) * force);
         heldObject.GetComponent<Rigidbody>().AddForce(GameObject.Find("FirstPersonCharacter").transform.up * force/2);
-        heldObject.GetComponent<Rigidbody>().useGravity = true;
-        
+
+        // Object reverts to its original properties: 
+        ObjectRevert();
+
         // object no longer held
         heldObject = null;
+
+
+    }
+
+    void StoreObjectData()
+    {
+        try
+        {
+            if (heldObject.GetComponent<LookAt>().enabled)
+            {
+                hod.lookAt = true;
+            }
+            else
+            {
+                hod.lookAt = false;
+            }
+        }
+        catch
+        {
+            Debug.Log("Issue in storing held object data");
+        }
+        try
+        {
+            if (heldObject.GetComponent<LookAt>().enabled && heldObject.GetComponent<LookAt>().lockToPlayer)
+            {
+                hod.lookAtPlayer = true;
+            }
+            else
+            {
+                hod.lookAtPlayer = false;
+            }
+        }
+        catch
+        {
+            Debug.Log("Issue in storing held object data");
+        }
+    }
+
+    void ObjectRevert()
+    {
+        heldObject.GetComponent<Rigidbody>().useGravity = true;
+
+        if (hod.lookAt)
+        {
+            heldObject.GetComponent<LookAt>().enabled = true;
+        }
+        if(hod.lookAtPlayer)
+        {
+            heldObject.GetComponent<LookAt>().lockToPlayer = true;
+        } else
+        {
+            heldObject.GetComponent<LookAt>().lockToPlayer = false;
+        }
+
+        // orient normally
+        if(heldObject.transform.position.y < -5f)
+        {
+            heldObject.transform.position = new Vector3(heldObject.transform.position.x, -4, heldObject.transform.position.z);
+        }
+        heldObject.transform.rotation = new Quaternion(0,0,0,0);
     }
 }
