537693b44280f9ae47d5b56e3e20b454143f1f4a
Petri Autio <petri.mikael.autio@gmail.com>
2017-12-28 10:37:13 +0000

    Front and backside awareness for sprites

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -6,15 +6,43 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    public List<GameObject> seeds;
+    // Need a weather manager 
+    // Traffic, road with a few cars
+    // Plenty of vegetation 
 
+    public List<GameObject> seeds;
+    public GameObject sun;
 	// Use this for initialization
 	void Start () {
-		
+        sun = GameObject.Find("SunLight");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKeyDown(KeyCode.K))
+        {
+            Dusk();
+        }
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            Day();
+        }
 	}
+     
+    // dawn, day, dusk, night
+    // directional light intensity should be adjusted, maybe also hue
+    // should be synched to the sun and moons
+    // -> Resting area
+    // 
+
+    void Dusk()
+    {
+        sun.GetComponent<Light>().intensity = 0.1f;
+    }
+
+    void Day()
+    {
+        sun.GetComponent<Light>().intensity = 1.0f;
+    }
+
 }
---
+++ Assets/Scripts/Perspective.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perspective : MonoBehaviour {
+    public GameObject front;
+    public GameObject back;
+    public bool frontCheck = true;
+    // Use this for initialization
+	void Start () {
+		
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if (frontCheck)
+            {
+                front.SetActive(true);
+                back.SetActive(false);
+            } else
+            {
+                front.SetActive(false);
+                back.SetActive(true);
+            }
+        }
+    }
+
+}
